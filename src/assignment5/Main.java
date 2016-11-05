57313e1861472893d9479ce0d67ced5f4a7cfc30
Rebecca Ho <rebeccaho936@gmail.com>
2016-11-05 17:26:41 -0500

    Added labels, textfields, and buttons for step, display, and make.

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -15,12 +15,18 @@ package assignment5;
 //import java.awt.Font;
 import java.lang.String;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -34,30 +40,112 @@ public class Main extends Application {
 			
 			//grid.setGridLinesVisible(true);
 			grid.setAlignment(Pos.BASELINE_LEFT);
-			grid.setHgap(1);
-			grid.setVgap(1);
+			grid.setHgap(5);
+			grid.setVgap(2);
 			grid.setPadding(new Insets(1, 1, 1, 1));
-			Scene scene = new Scene(grid, 500, 500);
+			Scene scene = new Scene(grid, 700, 700);
 			primaryStage.setScene(scene);
 			
 			
 			Text welcome = new Text("Welcome to Critters!");
 			welcome.setFont(Font.font("Arial",FontWeight.BOLD,14));
-			grid.add(welcome, 0, 0,2,1);
-			//GridPane.setConstraints(welcome,1,1);
+			grid.add(welcome, 0, 0, 2, 1);
 			
-			
+			/** make */
 			Label make = new Label("Add Critters");
-			//GridPane.setConstraints(make,1,100);
 			grid.add(make, 0, 10);
+			Label critterName = new Label("Critter Name:");
+			grid.add(critterName, 0, 11);
+			TextField nameField = new TextField();
+			grid.add(nameField, 1, 11);
+			Label critterNum = new Label("Number:");
+			grid.add(critterNum, 2, 11);
+			TextField numField = new TextField();
+			grid.add(numField, 3, 11);
+			numField.setMaxWidth(60);
+			Button makeBtn = new Button();
+	        makeBtn.setText("Make Critter(s)");
+	        Text wrongInput = new Text("Invalid Number!");
+			wrongInput.setFont(Font.font("Arial",FontWeight.NORMAL,12));
+			wrongInput.setFill(Color.RED);
 			
-			//grid.getChildren().addAll(welcome,make);
-			//new WorldDisplay(); // this will only happen if the user chooses displayWorld... added later
-			primaryStage.show();
+	        makeBtn.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	if(grid.getChildren().contains(wrongInput)) {
+	            		grid.getChildren().remove(wrongInput);
+	            	}
+	            	String name;
+	            	String num;
+	            	if((nameField.getText() != null && !nameField.getText().isEmpty())) {
+	            		name = nameField.getText();
+	            	}
+	            	if((numField.getText() != null && !numField.getText().isEmpty())) {
+	            		num = numField.getText();
+	            		num = num.trim();
+	            		// need a try block for invalid number
+	            		try{
+	            			int numInt = Integer.parseInt(num);
+	            		} catch(NumberFormatException e) {
+	            			grid.add(wrongInput, 3, 15);
+	            		}
+	            	}
+	            }
+	        });
+	        grid.add(makeBtn, 7, 11);
 			
-			// Paints the icons.
-			//Painter.paint();
+			/** show */
+	        Label show = new Label("Display World:");
+			grid.add(show, 0, 30);
+	        Button displayBtn = new Button();
+	        displayBtn.setText("Show Critters");
+	        
+	        displayBtn.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	new WorldDisplay();
+	            }
+	        });
+	        grid.add(displayBtn, 1, 30);
 			
+			
+			/** step */
+			Label step = new Label("Perform Time Steps");
+			grid.add(step, 0, 50);
+			Label stepLabel = new Label("Number of Steps:");
+			grid.add(stepLabel, 0, 51);
+			TextField stepField = new TextField();
+			grid.add(stepField, 1, 51);
+			stepField.setMaxWidth(60);
+			Button stepBtn = new Button();
+	        stepBtn.setText("Perform Steps(s)");
+	        Text wrongStepInput = new Text("Invalid Number!");
+			wrongStepInput.setFont(Font.font("Arial",FontWeight.NORMAL,12));
+			wrongStepInput.setFill(Color.RED);
+			
+	        stepBtn.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	if(grid.getChildren().contains(wrongStepInput)) {
+	            		grid.getChildren().remove(wrongStepInput);
+	            	}
+	            	String num;
+	            	if((stepField.getText() != null && !stepField.getText().isEmpty())) {
+	            		num = stepField.getText();
+	            		num = num.trim();
+	            		// need a try block for invalid number
+	            		try{
+	            			int numInt = Integer.parseInt(num);
+	            		} catch(NumberFormatException e) {
+	            			grid.add(wrongStepInput, 1, 52);
+	            		}
+	            	}
+	            }
+	        });
+	        grid.add(stepBtn, 7, 51);
+			
+	        primaryStage.show();
+	        
 		} catch(Exception e) {
 			e.printStackTrace();		
 		}
